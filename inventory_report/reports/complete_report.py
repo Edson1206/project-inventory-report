3a69beab7bcec7560143b8aa497f9bcc04ea74bf
Edson Pereira <ed.1206@gmail.com>
2023-04-06 22:40:07 -0300

    feat: :sparkles: Create complete report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,19 @@
-class CompleteReport:
-    pass
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(self, productList):
+        simple_report_result = SimpleReport.generate(productList)
+        list_companies = Counter(
+            [row["nome_da_empresa"] for row in productList]
+        )
+        complete_report_result = ""
+        for company, quantity in list_companies.items():
+            complete_report_result += f"- {company}: {quantity}\n"
+        return (
+            f"{simple_report_result}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{complete_report_result}"
+        )
