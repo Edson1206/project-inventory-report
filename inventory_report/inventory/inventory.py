462a3940f3202637b4056ac4592c8ec0429db135
Edson Pereira <ed.1206@gmail.com>
2023-04-11 00:06:31 -0300

    feat: :sparkles: Implemented json relatory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -8,16 +9,22 @@ class Inventory:
         data = []
         with open(path, encoding="utf8") as file:
             path_csv = csv.DictReader(file)
-
             for row in path_csv:
                 data.append(row)
         return data
+
+    def import_json(path):
+        with open(path) as file:
+            path_json = json.load(file)
+            return path_json
 
     @classmethod
     def import_data(cls, path, type):
         data = []
         if path.endswith(".csv"):
             data = Inventory.import_csv(path)
+        if path.endswith(".json"):
+            data = Inventory.import_json(path)
 
         if type == "simples":
             return SimpleReport.generate(data)
